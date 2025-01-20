dd4513078df6c67d75d11a6d54aa7af10cace8c5
tamerhayek <dev@tamerhayek.com>
2025-01-20 21:09:38 +0100

    feat: create window

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window():
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.title = "Maze Solver"
+		self.__root = Tk()
+		self.__root.title(self.title)
+		self.canvas = Canvas(self.__root, width=self.width, height=self.height)
+		self.canvas.pack(fill=BOTH)
+		self.running = False
+		self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+	def redraw(self):
+		self.__root.update_idletasks()
+		self.__root.update()
+
+	def wait_for_close(self):
+		self.running = True
+		while self.running:
+			self.redraw()
+
+	def close(self):
+		self.running = False
